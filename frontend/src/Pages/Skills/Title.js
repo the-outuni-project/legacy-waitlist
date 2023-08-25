4a9f2811d0b561d3f011fe89a86dcfc2b42155e5
samuelgrant <samueljegrant@outlook.com>
2023-08-26 09:58:22 +1200

    Disable: Elite/Gold skill page on starter hulls

--- frontend/src/Pages/Skills/Title.js
+++ frontend/src/Pages/Skills/Title.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { useHistory, useLocation } from "react-router-dom";
 import { Button, Buttons } from "../../Components/Form";
 import SkillsHelp from "./SkillsHelp";
+import { useEffect } from "react";
 
 const TitleDOM = styled.div`
   box-sizing: border-box;
@@ -18,6 +19,8 @@ const TitleDOM = styled.div`
   }
 `;
 
+const StarterHulls = ['Megathron', 'Apocalypse Navy Issue'];
+
 const Title = ({ hull, mastery }) => {
   const queryParams = new URLSearchParams(useLocation().search);
   const history = useHistory();
@@ -29,6 +32,17 @@ const Title = ({ hull, mastery }) => {
     });
   }
 
+
+  const IsStarterHull = StarterHulls.includes(hull.replace('+', ' '));
+  const Tooltip = `The ${hull.replace('+', ' ')} is a starter ship and <br />does not have Elite or Elite Gold skills.`;
+
+  useEffect(() => {
+    if (IsStarterHull && (mastery !== 'basic' || mastery !== 'min')) {
+      onClick('basic');
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [IsStarterHull, mastery ])
+
   return (
     <>
       <TitleDOM>
@@ -36,9 +50,32 @@ const Title = ({ hull, mastery }) => {
           <h2>{hull}</h2>
         </div>
         <Buttons>
-          <Button variant={mastery === 'basic' || mastery === 'min' ? 'primary' : null} onClick={e => onClick('basic')}>Basic</Button>
-          <Button variant={mastery === 'elite' ? 'primary' : null} onClick={e => onClick('elite')}>Elite</Button>
-          <Button variant={mastery === 'gold' ? 'primary' : null} onClick={e => onClick('gold')}>Gold</Button>
+          <Button
+            variant={mastery === 'basic' || mastery === 'min' ? 'primary' : null}
+            onClick={e => onClick('basic')}
+          >
+            Basic
+          </Button>
+
+          <Button
+            variant={mastery === 'elite' ? 'primary' : null}
+            onClick={e => onClick('elite')}
+            disabled={IsStarterHull}
+            data-tooltip-id={IsStarterHull ? 'tip' : null}
+            data-tooltip-html={IsStarterHull ? Tooltip : null}
+          >
+            Elite
+          </Button>
+
+          <Button
+            variant={mastery === 'gold' ? 'primary' : null}
+            onClick={e => onClick('gold')}
+            disabled={IsStarterHull}
+            data-tooltip-id={IsStarterHull ? 'tip' : null}
+            data-tooltip-html={IsStarterHull ? Tooltip : null}
+          >
+            Gold
+          </Button>
         </Buttons>
       </TitleDOM>
       <SkillsHelp />
